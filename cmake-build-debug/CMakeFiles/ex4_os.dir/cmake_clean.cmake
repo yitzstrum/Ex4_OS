e375c0e4d14c340fb40310c898b4251817975ae0
Yitzchak Strumberger <yitzstrum@cs.huji.ac.il>
2022-06-12 15:41:26 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/ex4_os.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4_os.dir/main.cpp.o"
+  "ex4_os"
+  "ex4_os.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex4_os.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ex4_os.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ex4_os.
+# This may be replaced when dependencies are built.
